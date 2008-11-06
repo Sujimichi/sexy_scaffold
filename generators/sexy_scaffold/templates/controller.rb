75dc88d1c8abdb35ae79e5ec71528cfda1a73778
wolas <jcpenche@gmail.com>
2008-11-06 13:50:26 +0000

    more variable name fixes

--- generators/sexy_scaffold/templates/controller.rb
+++ generators/sexy_scaffold/templates/controller.rb
@@ -7,24 +7,24 @@ class <%= controller_class_name %>Controller < ApplicationController
 
   # GET /<%= controller_plural_name %>/1
   def show
-    @<%= controller_singular_name %> = <%= name.capitalize %>.find(params[:id])
+    @<%= name %> = <%= name.capitalize %>.find(params[:id])
   end
 
   # GET /<%= controller_plural_name %>/new
   def new
-    @<%= controller_singular_name %> = <%= name.capitalize %>.new
+    @<%= name %> = <%= name.capitalize %>.new
   end
 
   # GET /<%= controller_plural_name %>/1/edit
   def edit
-    @<%= controller_singular_name %> = <%= name.capitalize %>.find(params[:id])
+    @<%= name %> = <%= name.capitalize %>.find(params[:id])
   end
 
   # POST /<%= controller_plural_name %>
   def create
-    @<%= controller_singular_name %> = <%= name.capitalize %>.new(params[:<%= name %>])
+    @<%= name %> = <%= name.capitalize %>.new(params[:<%= name %>])
 
-    if @<%= controller_singular_name %>.save
+    if @<%= name %>.save
       flash[:notice] = '<%= name.capitalize %> was successfully created.'
       redirect_to(@<%= name %>)
     else
@@ -34,11 +34,11 @@ class <%= controller_class_name %>Controller < ApplicationController
 
   # PUT /<%= controller_plural_name %>/1
   def update
-    @<%= controller_singular_name %> = <%= name.capitalize %>.find(params[:id])
+    @<%= name %> = <%= name.capitalize %>.find(params[:id])
 
-    if @<%= controller_singular_name %>.update_attributes(params[:<%= name %>])
+    if @<%= name %>.update_attributes(params[:<%= name %>])
       flash[:notice] = '<%= name.capitalize %> was successfully updated.'
-      redirect_to(@<%= controller_singular_name %>)
+      redirect_to(@<%= name %>)
     else
       render :action => "edit"
     end
@@ -46,8 +46,8 @@ class <%= controller_class_name %>Controller < ApplicationController
 
   # DELETE /<%= controller_plural_name %>/1
   def destroy
-    @<%= controller_singular_name %> = <%= name.capitalize %>.find(params[:id])
-    @<%= controller_singular_name %>.destroy
+    @<%= name %> = <%= name.capitalize %>.find(params[:id])
+    @<%= name %>.destroy
 
     redirect_to(<%= controller_plural_name %>_url)
   end
